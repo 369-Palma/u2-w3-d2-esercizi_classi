a0828797f669b801a86f6c2ea65edba1dd229423
Palma Iacobelli <118762347+369-Palma@users.noreply.github.com>
2023-01-24 18:04:21 +0100

    .

---
+++ assets/es2.js
@@ -0,0 +1,11 @@
+class Paginazione {
+  constructor(items, pageSize) {
+    this.items = items;
+    this.pageSize = pageSize;
+  }
+}
+
+let items = [];
+/* let pageSize = 10;
+
+let newPage = new Pagina([], 10); */
